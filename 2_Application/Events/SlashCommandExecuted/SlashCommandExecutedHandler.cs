ee9eed673a85e24963262e27df6c4d72aef57412
Nikita <nekitosik.pav@gmail.com>
2025-11-17 14:09:54 +0700

    -

--- 2_Application/Events/SlashCommandExecuted/SlashCommandExecutedHandler.cs
+++ 2_Application/Events/SlashCommandExecuted/SlashCommandExecutedHandler.cs
@@ -40,95 +40,102 @@ public class SlashCommandExecutedHandler(
 			{
 				case "set_lobby":
 
-					LobbyNameResponse lobbyNameResponse = await mediator.Send(new LobbyNameCommand()
 					{
-						LobbyName = notification.SocketSlashCommand.Data.Options.FirstOrDefault(x => x.Name == "name").Value.ToString() ?? string.Empty,
-						UserId = notification.SocketSlashCommand.User.Id
-					}, new());
+                        LobbyNameResponse lobbyNameResponse = await mediator.Send(new LobbyNameCommand()
+                        {
+                            LobbyName = notification.SocketSlashCommand.Data.Options.FirstOrDefault(x => x.Name == "name").Value.ToString() ?? string.Empty,
+                            UserId = notification.SocketSlashCommand.User.Id
+                        }, new());
 
-					await notification.SocketSlashCommand.RespondAsync(
-						embed: embedMessageExtension.CreateEmbed(new EmbedDto()
-						{
-							Description = lobbyNameResponse.Message,
-							Color = lobbyNameResponse.IsSuccess ? Discord.Color.Green : Discord.Color.Red
-						}),
-						ephemeral: false);
+                        await notification.SocketSlashCommand.RespondAsync(
+                            embed: embedMessageExtension.CreateEmbed(new EmbedDto()
+                            {
+                                Description = lobbyNameResponse.Message,
+                                Color = lobbyNameResponse.IsSuccess ? Discord.Color.Green : Discord.Color.Red
+                            }),
+                            ephemeral: false);
+                    }
 					break;
 
 				case "user_stat":
 
-					ulong targetUserId = notification.SocketSlashCommand.User.Id;
-					string targetUserName = notification.SocketSlashCommand.User.GlobalName;
-
-					if (notification.SocketSlashCommand.Data.Options.Any(x => x.Name == "user"))
 					{
-						if (notification.SocketSlashCommand.Data.Options.FirstOrDefault(x => x.Name == "user").Value is not SocketGuildUser optionUser || optionUser.IsBot)
-						{
-							await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new()
-							{
-								Description = "Пользователь не найден, либо пользователь бот",
-								Color = Discord.Color.Gold
-							}), ephemeral: true);
+                        ulong targetUserId = notification.SocketSlashCommand.User.Id;
+                        string targetUserName = notification.SocketSlashCommand.User.GlobalName;
 
-							return;
-						}
+                        if (notification.SocketSlashCommand.Data.Options.Any(x => x.Name == "user"))
+                        {
+                            if (notification.SocketSlashCommand.Data.Options.FirstOrDefault(x => x.Name == "user").Value is not SocketGuildUser optionUser || optionUser.IsBot)
+                            {
+                                await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new()
+                                {
+                                    Description = "Пользователь не найден, либо пользователь бот",
+                                    Color = Discord.Color.Gold
+                                }), ephemeral: true);
 
-						targetUserId = optionUser.Id;
-						targetUserName = optionUser.GlobalName;
-					}
+                                return;
+                            }
 
-					UserStatResponse userStatResponse = await mediator.Send(new UserStatCommand()
-					{
-						UserId = targetUserId,
-					}, new());
+                            targetUserId = optionUser.Id;
+                            targetUserName = optionUser.GlobalName;
+                        }
 
-					await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new EmbedDto()
-					{
-						Description = $"Общая статистика {targetUserName}\n" +
-						$"> Сообщений отправлено: **{(userStatResponse.MessageCount == -1 ? "-" : userStatResponse.MessageCount)}**\n" +
-						$"> Времени в голосовом канале: **{(userStatResponse.TotalSeconds != -1 ? userStatResponse.TotalSeconds / 3600 : 0)}h " +
-						$"{(userStatResponse.TotalSeconds != -1 ? userStatResponse.TotalSeconds % 3600 / 60 : 0)}m " +
-						$"{(userStatResponse.TotalSeconds != -1 ? userStatResponse.TotalSeconds % 3600 % 60 : 0)}s**",
-						Color = new(87, 206, 255)
+                        UserStatResponse userStatResponse = await mediator.Send(new UserStatCommand()
+                        {
+                            UserId = targetUserId,
+                        }, new());
 
-					}), ephemeral: true);
+                        await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new EmbedDto()
+                        {
+                            Description = $"Общая статистика {targetUserName}\n" +
+                            $"> Сообщений отправлено: **{(userStatResponse.MessageCount == -1 ? "-" : userStatResponse.MessageCount)}**\n" +
+                            $"> Времени в голосовом канале: **{(userStatResponse.TotalSeconds != -1 ? userStatResponse.TotalSeconds / 3600 : 0)}h " +
+                            $"{(userStatResponse.TotalSeconds != -1 ? userStatResponse.TotalSeconds % 3600 / 60 : 0)}m " +
+                            $"{(userStatResponse.TotalSeconds != -1 ? userStatResponse.TotalSeconds % 3600 % 60 : 0)}s**",
+                            Color = new(87, 206, 255)
 
+                        }), ephemeral: true);
+                    }
 					break;
 
 				case "authorize":
 
-					if (notification.SocketSlashCommand.User is SocketGuildUser user && !user.GuildPermissions.Administrator)
 					{
-						await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new()
-						{
-							Description = "Данную команду могут вызывать только администраторы сервера",
-							Color = Discord.Color.Red
-						}), ephemeral: true);
+                        if (notification.SocketSlashCommand.User is SocketGuildUser user && !user.GuildPermissions.Administrator)
+                        {
+                            await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new()
+                            {
+                                Description = "Данную команду могут вызывать только администраторы сервера",
+                                Color = Discord.Color.Red
+                            }), ephemeral: true);
 
-						return;
-					}
+                            return;
+                        }
 
-					AuResponse response = await mediator.Send(new AutorizeCommand()
-					{
-						User = notification.SocketSlashCommand.Data.Options.FirstOrDefault(x => x.Name == "user").Value as SocketGuildUser
-					}, new());
+                        AuResponse response = await mediator.Send(new AutorizeCommand()
+                        {
+                            User = notification.SocketSlashCommand.Data.Options.FirstOrDefault(x => x.Name == "user").Value as SocketGuildUser
+                        }, new());
 
-					await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new()
-					{
-						Description = response.Message,
-						Color = new Discord.Color(87, 206, 255)
-					}), ephemeral: true);
-
+                        await notification.SocketSlashCommand.RespondAsync(embed: embedMessageExtension.CreateEmbed(new()
+                        {
+                            Description = response.Message,
+                            Color = new Discord.Color(87, 206, 255)
+                        }), ephemeral: true);
+                    }
 					break;
 
 				default:
-					await notification.SocketSlashCommand.RespondAsync(
-					embed: embedMessageExtension.CreateEmbed(new EmbedDto()
-					{
-						Description = "Команда пока что в разработке",
-						Color = Discord.Color.Red
-					}),
-					ephemeral: true);
+
+                    {
+                        await notification.SocketSlashCommand.RespondAsync(
+                        embed: embedMessageExtension.CreateEmbed(new EmbedDto()
+                        {
+                            Description = "Команда пока что в разработке",
+                            Color = Discord.Color.Red
+                        }),
+                        ephemeral: true);
+                    }
 					break;
 			}
 		}
